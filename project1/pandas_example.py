a02c58155d2656331fbb3338d515ab7b57fefa13
irlperez <oscar.perez19197ja@gmail.com>
2022-11-06 18:12:19 -0600

    created pandas example. using pd to easily manipulate data

--- project1/pandas_example.py
+++ project1/pandas_example.py
@@ -3,20 +3,32 @@ from pathlib import Path
 import os
 
 # load data first
-directory = './data_sets'
-dfs = []
-for file in Path(directory).glob('**/*.csv'):
-    dfs.append(pd.read_csv(file))
+directory = './data_sets' 
+df_list = []
 
-df = pd.concat(dfs)
-print(df.columns)
-df.drop()
-print(df.columns)
+for file in Path(directory).glob('**/*.csv'):
+    df_temp = pd.read_csv(file)
+    df_list.append(df_temp)
+
+df = pd.concat(df_list)
+print(df.head())
 
 # drop column
+df = df.drop(['phone'], axis='columns')
+print(df.head())
+
 # adding extra column (concat first and last)
-# patition data
-# save data
+df['last, first'] = df['last_name'] + ', ' + df['first_name']
+print(df.head())
+
+# partition data and save data
+print(df.loc[df['age'] == 24])
+
+for age in df['age'].unique():
+    Path(f'./pd_partitioned_data/age={age}').mkdir(parents=True, exist_ok=True)
+    age_df = df.loc[df['age'] == age]
+    age_df.to_csv(f'./pd_partitioned_data/age={age}/data', header=True)
+
 
 # https://pandas.pydata.org/docs/user_guide/dsintro.html#dataframe
 # https://pandas.pydata.org/docs/user_guide/index.html
