696705ef80d498ad20d47aa7320da4e3ed52b539
irlperez <oscar.perez19197ja@gmail.com>
2022-11-09 21:52:22 -0600

    added .to_parquet which allows for data partitioning.

--- project1/pandas_example.py
+++ project1/pandas_example.py
@@ -29,6 +29,7 @@ for age in df['age'].unique():
     age_df = df.loc[df['age'] == age]
     age_df.to_csv(f'./pd_partitioned_data/age={age}/data', header=True)
 
+df.to_parquet('./pd_partitioned_data/df.parquet', partition_cols='age', engine='auto') # this is really cool!
 
 # https://pandas.pydata.org/docs/user_guide/dsintro.html#dataframe
 # https://pandas.pydata.org/docs/user_guide/index.html
